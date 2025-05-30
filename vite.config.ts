e279b3393e7e37727e721c019b47058a2082a9f5
Victor Fernando Silva Machado Evangelista <vfsmachado27@gmail.com>
2025-05-30 08:28:15 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PortfolioVictorFernando/', // precisa ser o NOME exato do repositório
   plugins: [react()],
-});
+  base: '/PortfolioVictorFernando/', // 👈 necessário para GitHub Pages
+})
